5479d248e1a380964cd2192a7f0c5b2a3812a47d
Abby BiYing <abbyzhangnyc@gmail.com>
2014-11-26 14:03:42 -0500

    add user index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require "monban/constraints/signed_out"
 
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :users, only: [:new, :create, :show, :edit, :update, :index]
   resources :images
   resources :locations, only: [:show, :create]
   resources :maps, only: [:show]
