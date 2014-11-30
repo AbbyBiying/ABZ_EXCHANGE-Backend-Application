e3026fb69f7428674c7d2f1307d3e831017c43f2
Abby BiYing <abbyzhangnyc@gmail.com>
2014-11-29 23:41:03 -0500

    add locations controller

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,29 @@
+class LocationsController < ApplicationController
+  def create
+    location = Location.new(location_params)
+    location.name = location.name.downcase
+    location.city = location.city.downcase
+    location.state = location.state.downcase
+
+    if location.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def show
+    @location = Location.find(params[:id])
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(
+      :name,
+      :state,
+      :latitude,
+      :longitude
+      )
+  end
+end
