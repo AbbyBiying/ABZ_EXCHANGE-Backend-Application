2381c952c4966f6c3e94598082dd0a2e08d99a3f
Abby BiYing <abbyzhangnyc@gmail.com>
2014-11-30 14:24:32 -0500

    add country to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141130045509) do
+ActiveRecord::Schema.define(version: 20141130191249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20141130045509) do
     t.integer  "number"
     t.string   "street"
     t.string   "full_street_address"
+    t.string   "country"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
