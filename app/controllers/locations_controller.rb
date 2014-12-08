ef8342373e4c5e8a89fd10e641b82eab743a1a04
Abby BiYing <abbyzhangnyc@gmail.com>
2014-12-08 15:40:39 -0500

    put google map up to location index

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -9,6 +9,15 @@ class LocationsController < ApplicationController
 
   def index
     @locations = Location.all
+    @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
+      marker.lat location.latitude
+      marker.lng location.longitude
+      marker.picture(
+        "url" => "http://bit.ly/1vFJs1n",
+        "width" => 32,
+        "height" => 32
+      )
+    end
   end
 
   def create
