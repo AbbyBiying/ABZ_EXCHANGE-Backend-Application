ac85a0a4f8db49d023406b8ef56867fbada35aed
Abby BiYing <abbyzhangnyc@gmail.com>
2015-01-16 18:53:50 -0500

    add state

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,12 +1,16 @@
 class Location < ActiveRecord::Base
   has_many :users
-  geocoded_by :city
+  geocoded_by :full_street_address
   after_validation :geocode
 
-  validates :city, presence: true, uniqueness: true
+  validates :city, presence: true
   validates :state, presence: true
 
   def self.by_most_recent
     order(created_at: :desc)
+  end
+
+  def full_street_address
+    "#{city}, #{state}"
   end
 end
